346d34c4f60ff66eda3fff526e6e3bcea61e314b
unknown <marco.luci95@gmail.com>
2023-03-28 12:42:59 -0400

    changes cursor on compare button

--- src/pages/Raptors/Raptors.js
+++ src/pages/Raptors/Raptors.js
@@ -24,9 +24,39 @@ function Raptors() {
   //   });
   // });
 
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+  // function myFunction() {
+  //   document.getElementById("myDropdown").classList.toggle("show");
+  // }
+
+  // // Close the dropdown menu if the user clicks outside of it
+  // window.onclick = function (event) {
+  //   if (!event.target.matches(".dropbtn")) {
+  //     const dropdowns = document.getElementsByClassName("dropdown-content");
+  //     let i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.classList.contains("show")) {
+  //         openDropdown.classList.remove("show");
+  //       }
+  //     }
+  //   }
+  // };
+
   return (
     <section className="main-section">
       <h2 className="main-section__header">Toronto Raptors</h2>
+      {/* <div class="dropdown">
+        <button onclick={myFunction()} className="drop-button">
+          Select player
+        </button>
+        <div id="my-dropdown" className="dropdown-content">
+          <p>Link 1</p>
+          <p>Link 2</p>
+          <p>Link 3</p>
+        </div>
+      </div> */}
       <div className="card-section">
         <div className="card-container">
           <div className="card">
