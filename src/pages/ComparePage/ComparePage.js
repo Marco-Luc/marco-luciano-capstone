39eca0ece0f67aa82776feb170e3b0a513367633
unknown <marco.luci95@gmail.com>
2023-03-29 15:59:46 -0400

    starts styling compare page

--- src/pages/ComparePage/ComparePage.js
+++ src/pages/ComparePage/ComparePage.js
@@ -1,9 +1,128 @@
 import "./ComparePage.scss";
+import fredVanvleet from "../../assets/Images/RaptorsPlayers/fvv.jpg";
+import kawhiClip from "../../assets/Videos/RaptorsVideos/KawhiClip.mp4";
 
 function ComparePage() {
   return (
-    <section className="main-section">
-      <h2 className="main-section__header">Compare Page</h2>
+    <section className="compare-page">
+      <h2 className="compare-page__header">Compare Stats</h2>
+      <div className="compare-section">
+        <div className="card-container">
+          <div className="card">
+            <div className="card__front">
+              <h2 className="card__front__title">Fred Vanvleet</h2>
+              <img
+                className="card__image"
+                src={fredVanvleet}
+                alt="Fred VanVleet"
+              />
+              <p className="card__front__description">Guard, Toronto Raptors</p>
+              <p className="card__front__quote">"Mr. Bet On Yourself"</p>
+            </div>
+            <div className="card__back">
+              <h2 className="card__stats-title">Stats</h2>
+              <div className="card__stats-container">
+                <ul className="card__stats">
+                  <li className="card__stats-stat">PPG: 25.3</li>
+                  <li className="card__stats-stat">APG: 7.8</li>
+                  <li className="card__stats-stat">RPG: 7.7</li>
+                  <li className="card__stats-stat">SPG: 1</li>
+                  <li className="card__stats-stat">BPG: 2</li>
+                </ul>
+                <video className="card__back__video" poster controls>
+                  <source src={kawhiClip}></source>
+                </video>
+                <p className="card__back__bio">
+                  Fred VanVleet is a well-rounded basketball player with a
+                  versatile skill set. He excels at shooting from beyond the
+                  arc, playing tough defense, and creating opportunities for
+                  himself and his teammates. His strong work ethic and
+                  leadership skills make him a valuable asset to any team, and
+                  he was a key player in the Toronto Raptors' championship run
+                  in 2019.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="compare-section__modal">
+          <p className="compare-section__modal-text">Testing</p>
+        </div>
+        <div className="card-container">
+          <div className="card">
+            <div className="card__front">
+              <h2 className="card__front__title">Fred Vanvleet</h2>
+              <img
+                className="card__image"
+                src={fredVanvleet}
+                alt="Fred VanVleet"
+              />
+              <p className="card__front__description">Guard, Toronto Raptors</p>
+              <p className="card__front__quote">"Mr. Bet On Yourself"</p>
+            </div>
+            <div className="card__back">
+              <h2 className="card__stats-title">Stats</h2>
+              <div className="card__stats-container">
+                <ul className="card__stats">
+                  <li className="card__stats-stat">PPG: 25.3</li>
+                  <li className="card__stats-stat">APG: 7.8</li>
+                  <li className="card__stats-stat">RPG: 7.7</li>
+                  <li className="card__stats-stat">SPG: 1</li>
+                  <li className="card__stats-stat">BPG: 2</li>
+                </ul>
+                <video className="card__back__video" poster controls>
+                  <source src={kawhiClip}></source>
+                </video>
+                <p className="card__back__bio">
+                  Fred VanVleet is a well-rounded basketball player with a
+                  versatile skill set. He excels at shooting from beyond the
+                  arc, playing tough defense, and creating opportunities for
+                  himself and his teammates. His strong work ethic and
+                  leadership skills make him a valuable asset to any team, and
+                  he was a key player in the Toronto Raptors' championship run
+                  in 2019.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="compare-page__dropdown-menus">
+        <select
+          className="compare-page__dropdown-menus__raptors"
+          type="text"
+          name="category"
+          value="raptors"
+          placeholder="Please select"
+          onChange="temporary"
+        >
+          <option>Please select</option>
+          <option>Fred Vanvleet</option>
+          <option>Gary Trent Jr.</option>
+          <option>OG Anunoby</option>
+          <option>Scottie Barnes</option>
+          <option>Pascal Siakam</option>
+        </select>
+        <select
+          className="compare-page__dropdown-menus__celtics"
+          type="text"
+          name="category"
+          value="celtics"
+          placeholder="Please select"
+          onChange="temporary"
+        >
+          <option>Please select</option>
+          <option>Jaylen Brown</option>
+          <option>Jayson Tatum</option>
+          <option>Al Horford</option>
+          <option>Robert Williams III</option>
+        </select>
+      </div>
+      <div className="compare-page__button-section">
+        <button className="compare-page__button-section-button">
+          Compare Stats
+        </button>
+      </div>
     </section>
   );
 }
