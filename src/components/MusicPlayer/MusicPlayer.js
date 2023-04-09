c22852e62cb24ccbec9f3e3763b1af64c37170b6
unknown <marco.luci95@gmail.com>
2023-04-08 22:25:04 -0400

    starts music player

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,23 +1,15 @@
 import "./MusicPlayer.scss";
-import { useState } from "react";
+import PlayerDetails from "../PlayerDetails/PlayerDetails";
 
-function MusicPlayer() {
-  const [songs, setSongs] = useState([
-    {
-      title: "Shadow",
-      artist: "The Worst Band Ever",
-      src: "../../assets/Music/01 Shadow.m4a",
-    },
-  ]);
-
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
-
+function MusicPlayer(props) {
   return (
     <div className="player">
       <audio></audio>
       <h4 className="player__heading">Playing Now</h4>
-      <p className="player__subheading">Beats by Marco Luciano</p>
+      <PlayerDetails song={props.song} />
+      <p className="player__subheading">
+        {props.nextSong.title} by {props.nextSong.artist}
+      </p>
     </div>
   );
 }
